aeacc215a6b7741ff7282bec6f1f1f9ff86a09d7
Erick John Carlo D. Solibet <carlo.solibet@gmail.com>
2022-09-01 01:58:11 +0800

    Add files via upload
    - initial file of forecaster. 
    - only to test streamlit sidebar and selectbox

---
+++ lica_forecast.py
@@ -0,0 +1,128 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep  1 01:10:34 2022
+
+@author: carlo
+"""
+
+# Essential libraries
+# =============================================================================
+import numpy as np
+import pandas as pd
+import seaborn as sns
+from io import BytesIO
+import openpyxl, requests
+import matplotlib.pyplot as plt
+from datetime import date, timedelta
+import streamlit as st
+
+# Modelling and Forecasting
+# =============================================================================
+from prophet import Prophet
+from prophet.diagnostics import cross_validation
+from prophet.diagnostics import performance_metrics
+from prophet.utilities import regressor_coefficients
+
+import itertools
+from sklearn.metrics import mean_squared_error
+import warnings
+warnings.filterwarnings(action='ignore', category=UserWarning)
+
+# Google trends
+from pytrends.request import TrendReq
+# save models
+from joblib import dump, load
+
+def sunday_of_calendarweek(year, week):
+    '''
+    Obtain date of sunday for the given calendar week
+    
+    Parameters
+    ----------
+    year : int
+    week : int
+    
+    Returns
+    -------
+    date string of sunday
+    '''
+    first = date(year, 1, 1)
+    base = 1 if first.isocalendar()[1] == 1 else 8
+    return first + timedelta(days=(base - first.isocalendar()[2] + 7 * (week - 1) - 1))
+
+def ratio(a, b):
+    '''
+    Function for calculating ratios to avoid inf
+    '''
+    return a/b if b else 0
+
+
+def get_data():
+    
+    # Data download
+    # =========================================================================
+    sheet_id = "17Yb2nYaf_0KHQ1dZBGBJkXDcpdht5aGE"
+    sheet_name = 'summary'
+    url = "https://docs.google.com/spreadsheets/export?exportFormat=xlsx&id=" + sheet_id
+    res = requests.get(url)
+    data_ = BytesIO(res.content)
+    xlsx = openpyxl.load_workbook(filename=data_)
+    traffic_data_ = pd.read_excel(data_, sheet_name = sheet_name)
+    
+    # Data preparation
+    # =========================================================================
+    
+    traffic_data_ = traffic_data_.dropna(axis=1)
+    traffic_data_ = traffic_data_.rename(columns={'Unnamed: 0': 'date'})
+    traffic_data_.loc[:, 'month'] = pd.to_datetime(traffic_data_.loc[:,'date']).dt.month
+    traffic_data_.loc[:, 'year'] = traffic_data_.loc[:, 'date'].dt.year
+    traffic_data_.loc[:, 'month_day'] = traffic_data_.loc[:,'date'].dt.day.values
+    traffic_data_.loc[:, 'weekday'] = traffic_data_.loc[:,'date'].dt.dayofweek + 1
+    traffic_data_.loc[:, 'week_of_month'] = traffic_data_.loc[:, 'date'].apply(lambda d: (d.day-1) // 7 + 1)
+    traffic_data_.loc[:, 'week_number'] = traffic_data_.apply(lambda x: int(x['date'].strftime('%U')), axis=1)
+    traffic_data_.loc[:, 'week_first_day'] = traffic_data_.apply(lambda x: sunday_of_calendarweek(int(x['year']), int(x['week_number'])), axis=1)
+    traffic_data_ = traffic_data_.set_index('date')
+    traffic_data_ = traffic_data_.asfreq('1D')
+    traffic_data_ = traffic_data_.sort_index()
+    
+    # Data engineering
+    # =========================================================================
+
+    clicks_cols = ['link_clicks_ga', 'link_clicks_fb']
+    impressions_cols = ['impressions_ga', 'impressions_fb']
+    purchases_backend_cols = [col for col in traffic_data_.columns if 'purchases_backend' in col]
+    
+    traffic_data_.loc[:, 'clicks_total'] = traffic_data_.loc[:,clicks_cols].sum(axis=1)
+    traffic_data_.loc[:, 'impressions_total'] = traffic_data_.loc[:,impressions_cols].sum(axis=1)
+    traffic_data_.loc[:, 'purchases_backend_total'] = traffic_data_.loc[:,purchases_backend_cols].sum(axis=1)
+    traffic_data_.loc[:, 'purchases_backend_marketplace'] = traffic_data_.loc[:, 'purchases_backend_fb'] + traffic_data_.loc[:, 'purchases_backend_shopee'] + traffic_data_.loc[:, 'purchases_backend_lazada']
+    traffic_data_.loc[:, 'purchases_backend_b2b'] = traffic_data_.loc[:, 'purchases_backend_b2b'] + traffic_data_.loc[:, 'purchases_backend_walk-in']
+    traffic_data_.drop(labels = ['purchases_backend_shopee', 'purchases_backend_lazada', 
+                                 'purchases_backend_fb', 'purchases_backend_walk-in', 
+                                 'purchases_backend_nan'], axis=1, inplace=True)
+    
+    traffic_data_.loc[:, 'ctr_ga'] = traffic_data_.apply(lambda x: ratio(x['link_clicks_ga'], x['impressions_ga']), axis=1)
+    traffic_data_.loc[:, 'ctr_fb'] = traffic_data_.apply(lambda x: ratio(x['link_clicks_fb'], x['impressions_fb']), axis=1)
+    
+    return traffic_data_
+
+if __name__ == '__main__':
+    st.title('LICA Target Setting App')
+    st.markdown('''
+                This tool forecasts the future values of important metrics to be used
+                for target setting.
+                ''')
+    platform = st.sidebar.selectbox('Select platform',
+                                    ('Gulong.ph', 'Mechanigo.ph'))
+    
+    if platform == 'Gulong.ph':
+        param  = st.sidebar.selectbox('Metric to Forecast',
+                              ('sessions', 'website purchases'))
+    elif platform == 'Mechanigo.ph':
+        param  = st.sidebar.selectbox('Metric to Forecast',
+                              ('sessions', 'website bookings'))
+    
+    traffic_data = get_data(platform)
+    start_train = '2022-03-01'
+    end_train = traffic_data.index.max().strftime('%Y-%m-%d')
+    end_test = '2022-09-15'
