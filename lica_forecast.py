729417e2288752c129894dbb8d2596b8f95ab77d
Erick John Carlo D. Solibet <carlo.solibet@gmail.com>
2022-09-20 09:52:59 +0800

    Add files via upload

--- lica_forecast.py
+++ lica_forecast.py
@@ -822,9 +822,10 @@ if __name__ == '__main__':
                 # provide input field
                 for exog in exogs:
                         total = st.number_input('Select metric total over forecast period',
-                                               min_value = 0, 
-                                               max_value = max(exogs[exog])*1.5,
-                                               value = exogs[exog].tail(forecast_horizon).mean())
+                                               min_value = 0.0, 
+                                               max_value = max(data[data.date.isin(date_series.ds.values)][exog])*1.5,
+                                               value = exogs[exog].tail(forecast_horizon).mean(),
+                                               step = 0.01)
                         future.loc[:, exog] = np.full((forecast_horizon,), round(total/forecast_horizon, 3))
             
     start_forecast = st.sidebar.checkbox('Launch forecast',
