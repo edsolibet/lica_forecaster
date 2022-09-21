b5905a94c63ac99e8f66d286091626037466e8af
Erick John Carlo D. Solibet <carlo.solibet@gmail.com>
2022-09-21 13:52:02 +0800

    Add files via upload

--- lica_forecast.py
+++ lica_forecast.py
@@ -770,16 +770,16 @@ if __name__ == '__main__':
                             # if data input is total
                             total = st.number_input('Select {} total over forecast period'.format(exog),
                                                    min_value = 0.0, 
-                                                   value = sum(exog_data[-forecast_horizon:]),
+                                                   value = sum(exog_data[-int(forecast_horizon):]),
                                                    step = 0.01)
-                            future.loc[future.index[-forecast_horizon:],exog] = np.full((forecast_horizon,), round(total/forecast_horizon, 3))
+                            future.loc[future.index[-int(forecast_horizon):],exog] = np.full((int(forecast_horizon),), round(total/forecast_horizon, 3))
                         else:
                             # if data input is average
                             average = st.number_input('Select {} average over forecast period'.format(exog),
                                                    min_value = 0.0, 
-                                                   value = np.mean(exog_data[-forecast_horizon:]),
+                                                   value = np.mean(exog_data[-int(forecast_horizon):]),
                                                    step = 0.01)
-                            future.loc[future.index[-forecast_horizon:],exog] = np.full((forecast_horizon,), round(average, 3))
+                            future.loc[future.index[-int(forecast_horizon):],exog] = np.full((int(forecast_horizon),), round(average, 3))
             else:
                 # delete unused fields
                 regressor_input.empty()
